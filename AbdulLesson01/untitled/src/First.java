ac381d45190466d234be765b4b52567e7d531ce7
kaweeshwaraa <kaweeshwara0@gmail.com>
2026-06-23 11:15:14 +0530

    day01

---
+++ AbdulLesson01/untitled/src/First.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class First {
+    public static void main(String[] args){
+
+        Scanner scan = new Scanner(System.in);
+        String name ;
+
+        System.out.println("Please Enter your name");
+        name = scan.nextLine();
+
+        System.out.println("Welcome " + name);
+
+
+    }
+}
