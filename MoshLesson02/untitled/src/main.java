29090603152c2067cca9ab38e30f2b7a54d66731
kaweeshwaraa <kaweeshwara0@gmail.com>
2026-06-29 17:21:57 +0530

    day03

---
+++ MoshLesson02/untitled/src/main.java
@@ -0,0 +1,40 @@
+import java.awt.*;
+import java.util.Arrays;
+import java.util.Date;
+
+public class main {
+    public static void main(String[] args) {
+//        int age = 30;
+//
+//        Date now = new Date();
+//        System.out.println(now);
+
+//        Point point1 = new Point(2,3);
+//        Point point2 = point1;
+//        point1.x =  10;
+//        System.out.println(point1);
+
+//        String message = "Hello World";
+
+//        int [] numbers = new int[5];
+//        numbers[0] = 1;
+//        numbers[1] = 2;
+
+//        int [] numbers = {1,2,3,4};
+//
+//        System.out.println(Arrays.toString(numbers));
+
+//        int[][] numbers = new int [3][3];
+//
+//        numbers[0][0] = 1;
+//        System.out.println(Arrays.deepToString(numbers));
+
+//        int [][] numbers = {{1,2},{3,4}};
+//
+//        System.out.println(Arrays.deepToStrin  g(numbers));
+
+        final float PI = 2.35F;
+
+
+    }
+}
